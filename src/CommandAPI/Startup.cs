50884c8a4fa09258235166f5967f3e158f133104
Riad Khaldi <riad.khaldi@outlook.com>
2021-02-14 17:15:15 -0600

    Applied Dtos and completed CRUD operations

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Npgsql;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
 
 namespace CommandAPI
 {
@@ -36,7 +38,14 @@ namespace CommandAPI
             services.AddDbContext<CommandContext>
               (opt => opt.UseNpgsql(builder.ConnectionString));
 
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
             services.AddControllers();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
